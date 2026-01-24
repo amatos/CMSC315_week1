98933921cd12eae3463467fd253cf49240aec08d
Alberth Matos <alberth@matos.cc>
2026-01-24 00:57:55 -0500

    Updated latexmkrc
    Added excluded file

---
+++ week_2/src/quiz_21_2_1_2.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class quiz_21_2_1 {
+  public static void main(String[] args) {
+    Set<A> set = new HashSet<A>();
+    set.add(new A());
+    set.add(new A());
+    set.add(new A());
+    set.add(new A());
+    System.out.println(set);
+  }
+}
+
+class A {
+  int r = 1;
+
+  public String toString() {
+    return r + "";
+  }
+
+  public boolean equals(Object o) {
+    return this.r == ((A) o).r;
+  }
+
+  public int hashCode() {
+    return r;
+  }
+}
